47f66195f06dbb50eb7ba3da41649b577d170051
Anoop555777 <ajbisht99@gmail.com>
2023-01-16 20:55:58 +0530

    done

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
     name: "Airpods Wireless Bluetooth Headphones",
-    user: "637b98bd2bc8addef49c4cea",
+    user: "638ae0dabf68b804f82f24a7",
     image: "/images/airpods.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
@@ -12,7 +12,7 @@ const products = [
   },
   {
     name: "iPhone 11 Pro 256GB Memory",
-    user: "637b98bd2bc8addef49c4cea",
+    user: "638ae0dabf68b804f82f24a7",
     image: "/images/phone.jpg",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
@@ -24,7 +24,7 @@ const products = [
   },
   {
     name: "Cannon EOS 80D DSLR Camera",
-    user: "637b98bd2bc8addef49c4cea",
+    user: "638ae0dabf68b804f82f24a7",
     image: "/images/camera.jpg",
     description:
       "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
@@ -35,7 +35,7 @@ const products = [
   },
   {
     name: "Sony Playstation 4 Pro White Version",
-    user: "637b98bd2bc8addef49c4cea",
+    user: "638ae0dabf68b804f82f24a7",
     image: "/images/playstation.jpg",
     description:
       "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
@@ -46,7 +46,7 @@ const products = [
   },
   {
     name: "Logitech G-Series Gaming Mouse",
-    user: "637b98bd2bc8addef49c4cea",
+    user: "638ae0dabf68b804f82f24a7",
     image: "/images/mouse.jpg",
     description:
       "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
@@ -57,7 +57,7 @@ const products = [
   },
   {
     name: "Amazon Echo Dot 3rd Generation",
-    user: "637b98bd2bc8addef49c4cea",
+    user: "638ae0dabf68b804f82f24a7",
     image: "/images/alexa.jpg",
     description:
       "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
